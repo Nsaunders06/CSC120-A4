c3c8cc242df93dee5453922e0be636c4dc5ad839
Nina Saunders <nsaunders@smith.edu>
2024-12-12 12:25:43 -0500

    Comments

--- Passenger.java
+++ Passenger.java
@@ -1,18 +1,32 @@
 public class Passenger {
     private String name;
-
+/**
+ * Constructor for Passenger class 
+ * @param name
+ */
     public Passenger(String name) {
         this.name = name;
     }
-
+/**
+ * Gives you the passenger name 
+ * @return name 
+ */
     public String getName() {
         return this.name;
     }
-
+/**
+ * Allows you to add passenger to a car 
+ * @param c
+ * @return this 
+ */
     public Boolean boardCar (Car c) {
         return c.addPassenger(this);
     }
-
+/**
+ * Removes a passenger from a car 
+ * @param c
+ * @return this 
+ */
     public Boolean getOffCar(Car c) {
         return c.removePassenger(this);
     }
